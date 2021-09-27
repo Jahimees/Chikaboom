f6b95043cfc302ed51470a4388fc4852286ba94b
2Rman <i_rau90@.mail.ru>
2021-09-28 00:17:48 +0300

    B-3 Исправлена упущенное замечание по final в константе

--- src/main/java/net/chikaboom/constant/AttributeConstant.java
+++ src/main/java/net/chikaboom/constant/AttributeConstant.java
@@ -1,6 +1,5 @@
 package net.chikaboom.constant;
 
-//Если ты пишешь в верхнем регистре, подразуемевается, что это константы. Соответственно нужно добавить final
 public class AttributeConstant {
-    public static String COMMAND = "command";
+    public static final String COMMAND = "command";
 }
