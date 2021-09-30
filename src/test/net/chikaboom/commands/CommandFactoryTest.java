6fa0155936ace9c68f66abc6d1bec833a309642e
2Rman <i_rau90@.mail.ru>
2021-09-30 21:21:14 +0300

    B-3 Сделан тест на CommandFactory.defineCommand()

--- src/test/net/chikaboom/commands/CommandFactoryTest.java
+++ src/test/net/chikaboom/commands/CommandFactoryTest.java
@@ -3,34 +3,24 @@ package net.chikaboom.commands;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import javax.servlet.http.HttpServletResponse;
-
 class CommandFactoryTest {
 
     @Test
     void checkDefineCommand() {
-        /*Не получится без mock...
-        Потому что принимается HttpServletRequest
-        Поэтому надо создавать, как я понимаю, псевдоподключение, или, как минимум, псевдоREQUEST с методом POST
-        Но общий смысл вспомнил, должно быть что-то вроде такого (Фантазер!!!):
 
-        CommandFactory factory = new CommandFactory(); - должно заноситься в @BeforeAll,
-                                                                                    если будем проверять все команды
+        String testCommandNone = "";
+        String testCommandNull = null;
 
-        Создать_нечто,_из_чего_можно_извлечь_запрос() {
-            скорее всего куча
-            @Override
-            @Override
-            @Override
-            "http://chikaboom.com/view/main.jsp?command=null" тоже где-то скорее всего будет фигурировать
-        }
+        CommandFactory commandFactory = new CommandFactory();
 
-        И собственно суть:
+        Class actual = commandFactory.defineCommand(testCommandNone).getClass();
+        Class expected = new EmptyCommand().getClass();
 
-        String expected = "view/main.jsp";// или MAIN_PAGE; как именно будет правильно не уверен
-        String actual = factory.defineCommand(request, response);
+        Class actual1 = commandFactory.defineCommand(testCommandNull).getClass();
+        Class expected1 = new EmptyCommand().getClass();
 
         Assertions.assertEquals(expected, actual);
-        */
+        Assertions.assertEquals(expected1, actual1);
+
     }
 }
