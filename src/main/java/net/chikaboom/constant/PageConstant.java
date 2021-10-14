a96d547a3673e59fde83f4a7378251fa63c448f1
2Rman <i_rau90@.mail.ru>
2021-10-14 20:16:14 +0300

    B-9 Добавлена обарботка команды авторизации, в процессе реалзации

--- src/main/java/net/chikaboom/constant/PageConstant.java
+++ src/main/java/net/chikaboom/constant/PageConstant.java
@@ -2,5 +2,5 @@ package net.chikaboom.constant;
 
 public class PageConstant {
     public static final String MAIN_PAGE = "/view/main.jsp";
-    public static final String ACCOUNT_PAGE = "https://i.ytimg.com/vi/lAqf71knZF8/maxresdefault.jpg";
+    public static final String ACCOUNT_PAGE = "/view/stub.png";
 }
