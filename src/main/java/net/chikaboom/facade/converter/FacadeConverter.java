2decaf5e07319f26295b205db4ad6e66db0d8cb0
jahimees <antonovich.01@mail.ru>
2023-10-09 17:29:16 +0300

    XMS-129 Facade layer complete

--- src/main/java/net/chikaboom/facade/converter/FacadeConverter.java
+++ src/main/java/net/chikaboom/facade/converter/FacadeConverter.java
@@ -3,8 +3,27 @@ package net.chikaboom.facade.converter;
 import net.chikaboom.facade.dto.Facade;
 import net.chikaboom.model.database.BaseEntity;
 
+/**
+ * Предназначен для конвертации объектов из базы данных в объекты для отправки на клиент или наоборот
+ *
+ * @param <F> тип фасадного объекта для отправки на клиент
+ * @param <M> тип объекта базы данных - модель
+ */
 public interface FacadeConverter<F extends Facade, M extends BaseEntity> {
 
+    /**
+     * Конвертирует объект базы данных в объект фасада - DTO
+     *
+     * @param model объект модели
+     * @return объект фасада - DTO
+     */
     F convertToDto(M model);
+
+    /**
+     * Конвертирует объект фасада в объект модели
+     *
+     * @param facade объект фасада - DTO
+     * @return объект модели
+     */
     M convertToModel(F facade);
 }
